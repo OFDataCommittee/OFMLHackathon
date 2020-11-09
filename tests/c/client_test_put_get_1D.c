3dfdb48b5af4b0594b57496ced2e4374651880c9
Matthew Ellis <mellis@cray.com>
2020-11-09 11:49:52 -0800

    Update to the 1D put get c test.

--- tests/c/client_test_put_get_1D.c
+++ tests/c/client_test_put_get_1D.c
@@ -4,28 +4,31 @@
 #include <stdlib.h>
 #include <mpi.h>
 
-int put_get_1D_array_double(int* dims, int n_dims,
-                  char* key_suffix, int key_suffix_length)
+void put_get_1D_tensor(void* tensor, int* dims, int n_dims,
+                       void* result, char* type,
+                       int type_length,
+                       char* key_suffix,
+                       int key_suffix_length)
 {
+  /* This function is a data type agnostic put and
+  get for 1D tensors.  The result vector
+  is filled with the fetched tensor.  Accuracy
+  checking is done outside of this function because
+  the type is not known.
+  */
+
   void* client = SmartSimCClient(true);
-
-  //Allocate and fill arrays
-  double* array = (double*)malloc(dims[0]*sizeof(double));
-  double* result = (double*)malloc(dims[0]*sizeof(double));
-
-  for(int i=0; i<dims[0]; i++)
-    array[i] = ((double)rand())/RAND_MAX;
 
   int rank = 0;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
   if(rank > 9)
     printf("%s", "C test does not support MPI ranks "\
-                 "greater than 9.");
+                 "greater than 9.\n");
 
   char* prefix_str = "1D_tensor_test_rank_";
   char* rank_str = malloc(2*sizeof(char));
-  rank_str[0] = rank + '0';
+  rank_str[0] = rank + (int)'0';
   rank_str[1] = 0;
 
   size_t prefix_str_length = strlen(prefix_str);
@@ -38,35 +41,86 @@ int put_get_1D_array_double(int* dims, int n_dims,
   int pos;
   pos = 0;
   memcpy(&key[pos], prefix_str, prefix_str_length);
-  pos += prefix_str_length + 1;
+  pos += prefix_str_length;
   memcpy(&key[pos], rank_str, rank_str_length);
   pos += rank_str_length;
   memcpy(&key[pos], key_suffix, key_suffix_length);
   pos += key_suffix_length;
   key[pos] = 0;
 
-  char* type = "DOUBLE";
-  size_t type_length = strlen(type);
-
   put_tensor(client, key, key_length, type, type_length,
-             (void*)array, dims, n_dims);
+             (void*)tensor, dims, n_dims);
   get_tensor(client, key, key_length, type, type_length,
              result, dims, n_dims);
 
-  for(int i=0; i<dims[0]; i++) {
-    if(array[i]!=result[i]) {
-      printf("%s", "The arrays do not match!");
-      free(array);
-      free(result);
-      return -1;
-    }
-  }
-
   free(rank_str);
   free(key);
-  free(array);
+  DeleteCClient(client);
+}
+
+int put_get_1D_tensor_double(int* dims, int n_dims,
+                  char* key_suffix, int key_suffix_length)
+{
+  /* This function puts and gets a 1D tensor of double
+  values.  If the sent and received tensors do not match,
+  a non-zero value is returned.
+  */
+  double* tensor = (double*)malloc(dims[0]*sizeof(double));
+  double* result = (double*)malloc(dims[0]*sizeof(double));
+
+  for(int i=0; i<dims[0]; i++)
+    tensor[i] = ((double)rand())/RAND_MAX;
+
+  char* type = "DOUBLE";
+  size_t type_length = strlen(type);
+
+  put_get_1D_tensor(tensor, dims, n_dims, result,
+                    type, type_length, key_suffix,
+                    key_suffix_length);
+
+  int r_value = 0;
+  for(int i=0; i<dims[0]; i++) {
+    if(tensor[i]!=result[i]) {
+      printf("%s", "The tensors do not match!");
+      r_value = -1;
+    }
+  }
+  free(tensor);
   free(result);
-  return 0;
+  return r_value;
+}
+
+int put_get_1D_tensor_float(int* dims, int n_dims,
+                           char* key_suffix,
+                           int key_suffix_length)
+{
+  /* This function puts and gets a 1D tensor of float
+  values.  If the sent and received tensors do not match,
+  a non-zero value is returned.
+  */
+  float* tensor = (float*)malloc(dims[0]*sizeof(float));
+  float* result = (float*)malloc(dims[0]*sizeof(float));
+
+  for(int i=0; i<dims[0]; i++)
+    tensor[i] = ((float)rand())/RAND_MAX;
+
+  char* type = "DOUBLE";
+  size_t type_length = strlen(type);
+
+  put_get_1D_tensor(tensor, dims, n_dims, result,
+                    type, type_length, key_suffix,
+                    key_suffix_length);
+
+  int r_value = 0;
+  for(int i=0; i<dims[0]; i++) {
+    if(tensor[i]!=result[i]) {
+      printf("%s", "The tensors do not match!");
+      r_value = -1;
+    }
+  }
+  free(tensor);
+  free(result);
+  return r_value;
 }
 
 int main(int argc, char* argv[]) {
@@ -77,11 +131,16 @@ int main(int argc, char* argv[]) {
   dims[0] = 10;
   int n_dims = 1;
 
-  char* suffix = "_dbl_c";
-  size_t suffix_length = strlen(suffix);
+  int result;
+  //1D double tensor
+  char* dbl_suffix = "_dbl_c";
+  result =  put_get_1D_tensor_double(dims, n_dims,
+                      dbl_suffix, strlen(dbl_suffix));
 
-  int result =  put_get_1D_array_double(dims, n_dims,
-                                        suffix, suffix_length);
+  //1D float tensor
+  result =  put_get_1D_tensor_float(dims, n_dims,
+                      dbl_suffix, strlen(dbl_suffix));
+
   free(dims);
   MPI_Finalize();
   printf("%s","Test passed: ");
