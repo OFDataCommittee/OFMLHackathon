962c71d5fdf818c892d080d748fe9127e5590b7a
Andrew Shao <aeshao@gmail.com>
2020-12-09 16:16:01 -0800

    Fortran test: 1D array put/get
    Adds a test for the Fortran client that puts and gets a 1D array to and
    from the database. The sent and retrieved array are checked to ensure
    that they are identical.

---
+++ tests/fortran_oop/client_test_put_get_1D.F90
@@ -0,0 +1,78 @@
+program main
+
+  use mpi
+  use silc, only : silc_client => client
+
+  implicit none
+
+  integer, parameter :: dim1 = 10
+
+  real(kind=4),    dimension(dim1) :: recv_array_real_32
+  real(kind=8),    dimension(dim1) :: recv_array_real_64
+  integer(kind=1), dimension(dim1) :: recv_array_integer_8
+  integer(kind=2), dimension(dim1) :: recv_array_integer_16
+  integer(kind=4), dimension(dim1) :: recv_array_integer_32
+  integer(kind=8), dimension(dim1) :: recv_array_integer_64
+
+  real(kind=4),    dimension(dim1) :: true_array_real_32
+  real(kind=8),    dimension(dim1) :: true_array_real_64
+  integer(kind=1), dimension(dim1) :: true_array_integer_8
+  integer(kind=2), dimension(dim1) :: true_array_integer_16
+  integer(kind=4), dimension(dim1) :: true_array_integer_32
+  integer(kind=8), dimension(dim1) :: true_array_integer_64
+
+  integer :: i
+  type(silc_client) :: client
+
+  integer :: err_code, pe_id
+  character(len=9) :: key_prefix
+
+  call MPI_init( err_code )
+  call MPI_comm_rank( MPI_COMM_WORLD, pe_id, err_code)
+  write(key_prefix, "(A,I6.6)") "pe_",pe_id
+
+  call random_number(true_array_real_32)
+  call random_number(true_array_real_64)
+  do i=1,dim1
+    true_array_integer_8(i) = irand()
+    true_array_integer_16(i) = irand()
+    true_array_integer_32(i) = irand()
+    true_array_integer_64(i) = irand()
+  enddo
+
+  call random_number(recv_array_real_32)
+  call random_number(recv_array_real_64)
+  do i=1,dim1
+    recv_array_integer_8(i) = irand()
+    recv_array_integer_16(i) = irand()
+    recv_array_integer_32(i) = irand()
+    recv_array_integer_64(i) = irand()
+  enddo
+
+  call client%initialize(cluster=.true.)
+
+  call client%put_tensor(key_prefix//"true_array_real_32", true_array_real_32, shape(true_array_real_32))
+  call client%unpack_tensor(key_prefix//"true_array_real_32", recv_array_real_32, shape(recv_array_real_32))
+  if (.not. all(true_array_real_32 == recv_array_real_32)) stop 'true_array_real_32: FAILED'
+
+  call client%put_tensor(key_prefix//"true_array_real_64", true_array_real_64, shape(true_array_real_64))
+  call client%unpack_tensor(key_prefix//"true_array_real_64", recv_array_real_64, shape(recv_array_real_64))
+  if (.not. all(true_array_real_64 == recv_array_real_64)) stop 'true_array_real_64: FAILED'
+
+  call client%put_tensor(key_prefix//"true_array_integer_8", true_array_integer_8, shape(true_array_integer_8))
+  call client%unpack_tensor(key_prefix//"true_array_integer_8", recv_array_integer_8, shape(recv_array_integer_8))
+  if (.not. all(true_array_integer_8 == recv_array_integer_8)) stop 'true_array_integer_8: FAILED'
+
+  call client%put_tensor(key_prefix//"true_array_integer_16", true_array_integer_16, shape(true_array_integer_16))
+  call client%unpack_tensor(key_prefix//"true_array_integer_16", recv_array_integer_16, shape(recv_array_integer_16))
+  if (.not. all(true_array_integer_16 == recv_array_integer_16)) stop 'true_array_integer_16: FAILED'
+
+  call client%put_tensor(key_prefix//"true_array_integer_32", true_array_integer_32, shape(true_array_integer_32))
+  call client%unpack_tensor(key_prefix//"true_array_integer_32", recv_array_integer_32, shape(recv_array_integer_32))
+  if (.not. all(true_array_integer_32 == recv_array_integer_32)) stop 'true_array_integer_32: FAILED'
+
+  call client%put_tensor(key_prefix//"true_array_integer_64", true_array_integer_64, shape(true_array_integer_64))
+  call client%unpack_tensor(key_prefix//"true_array_integer_64", recv_array_integer_64, shape(recv_array_integer_64))
+  if (.not. all(true_array_integer_64 == recv_array_integer_64)) stop 'true_array_integer_64: FAILED'
+
+end program main
