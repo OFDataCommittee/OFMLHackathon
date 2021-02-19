e4ab34cbb3c4c3590e19ff3ae0b3050c69238a73
Matthew Ellis <matthew.ellis@hpe.com>
2021-02-19 10:39:29 -0800

    Update SILC cluster test environment in python.

--- tests/python/get_cluster_env.py
+++ tests/python/get_cluster_env.py
@@ -2,7 +2,6 @@ import os
 
 def cluster():
     cluster_env_var = os.environ["SILC_TEST_CLUSTER"]
-    cluster = False
     if cluster_env_var.lower() == "true":
-        cluster = True
-    return cluster
+        return True
+    return False
