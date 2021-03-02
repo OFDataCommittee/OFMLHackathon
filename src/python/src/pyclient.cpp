b4c013613c80fb828e5a2d3ed3a6e0afe29f6a07
Matthew Ellis <matthew.ellis@hpe.com>
2021-03-02 13:43:37 -0800

    Add try catch blocks to the python client to reraise all errors that inherit from std::exception to raise as a std::runtime_error for easy handling in the client.

--- src/python/src/pyclient.cpp
+++ src/python/src/pyclient.cpp
@@ -6,41 +6,55 @@ namespace py = pybind11;
 
 PyClient::PyClient(bool cluster)
 {
-  Client* client = new Client(cluster);
-  this->_client = client;
+    Client* client = new Client(cluster);
+    this->_client = client;
 }
 
-PyClient::~PyClient() {
-  delete this->_client;
+PyClient::~PyClient()
+{
+    delete this->_client;
 }
 
-void PyClient::put_tensor(std::string& key, std::string& type, py::array data) {
+void PyClient::put_tensor(std::string& key,
+                          std::string& type,
+                          py::array data)
+{
+    auto buffer = data.request();
+    void* ptr = buffer.ptr;
 
-  auto buffer = data.request();
-  void* ptr = buffer.ptr;
+    // get dims
+    std::vector<size_t> dims(buffer.ndim);
+    for (int i=0; i < buffer.shape.size(); i++) {
+        dims[i] = (size_t) buffer.shape[i];
+    }
 
-  // get dims
-  std::vector<size_t> dims(buffer.ndim);
-  for (int i=0; i < buffer.shape.size(); i++) {
-      dims[i] = (size_t) buffer.shape[i];
+    TensorType ttype = TENSOR_TYPE_MAP.at(type);
+
+    try {
+        this->_client->put_tensor(key, ptr, dims, ttype,
+                                    MemoryLayout::contiguous);
+    }
+    catch(const std::exception& e) {
+        throw std::runtime_error(e.what());
+    }
+    return;
+}
+
+py::array PyClient::get_tensor(std::string& key)
+{
+    // placeholder variables to be filled
+    TensorType type;
+    std::vector<size_t> dims;
+    void* ptr;
+
+    // call C++ client
+    try {
+        this->_client->get_tensor(key, ptr, dims, type,
+                                    MemoryLayout::contiguous);
+    }
+    catch(const std::exception& e) {
+        throw std::runtime_error(e.what());
   }
-
-  TensorType ttype = TENSOR_TYPE_MAP.at(type);
-  this->_client->put_tensor(key, ptr, dims, ttype,
-                            MemoryLayout::contiguous);
-  return;
-}
-
-py::array PyClient::get_tensor(std::string& key) {
-
-  // placeholder variables to be filled
-  TensorType type;
-  std::vector<size_t> dims;
-  void* ptr;
-
-  // call C++ client
-  this->_client->get_tensor(key, ptr, dims, type,
-                            MemoryLayout::contiguous);
 
   // detect data type
   switch(type) {
@@ -99,36 +113,66 @@ py::array PyClient::get_tensor(std::string& key) {
   }
 }
 
-void PyClient::put_dataset(PyDataset& dataset) {
-
-  this->_client->put_dataset(*dataset.get());
-  return;
+void PyClient::put_dataset(PyDataset& dataset)
+{
+    try {
+        this->_client->put_dataset(*dataset.get());
+    }
+    catch(const std::exception& e) {
+        throw std::runtime_error(e.what());
+    }
+    return;
 }
 
-PyDataset* PyClient::get_dataset(const std::string& name) {
-  DataSet* data = new DataSet(this->_client->get_dataset(name));
-  PyDataset* dataset = new PyDataset(*data);
-  return dataset;
+PyDataset* PyClient::get_dataset(const std::string& name)
+{
+    DataSet* data;
+    try {
+        data = new DataSet(this->_client->get_dataset(name));
+    }
+    catch(const std::exception& e) {
+        throw std::runtime_error(e.what());
+    }
+    PyDataset* dataset = new PyDataset(*data);
+    return dataset;
 }
 
 void PyClient::set_script_from_file(const std::string& key,
                                     const std::string& device,
                                     const std::string& script_file)
 {
-  this->_client->set_script_from_file(key, device, script_file);
-  return;
+    try {
+        this->_client->set_script_from_file(key, device, script_file);
+    }
+    catch(const std::exception& e) {
+        throw std::runtime_error(e.what());
+    }
+    return;
 }
 
 void PyClient::set_script(const std::string& key,
                           const std::string& device,
                           const std::string_view& script)
 {
-  this->_client->set_script(key, device, script);
-  return;
+    try {
+        this->_client->set_script(key, device, script);
+    }
+    catch(const std::exception& e) {
+        throw std::runtime_error(e.what());
+    }
+    return;
 }
 
-std::string_view PyClient::get_script(const std::string& key) {
-  return this->_client->get_script(key);
+std::string_view PyClient::get_script(const std::string& key)
+{
+    std::string_view script;
+    try {
+        script = this->_client->get_script(key);
+    }
+    catch(const std::exception& e) {
+        throw std::runtime_error(e.what());
+    }
+    return script;
 }
 
 void PyClient::run_script(const std::string& key,
@@ -136,14 +180,26 @@ void PyClient::run_script(const std::string& key,
                 std::vector<std::string>& inputs,
                 std::vector<std::string>& outputs)
 {
-  this->_client->run_script(key, function, inputs, outputs);
-  return;
+    try {
+      this->_client->run_script(key, function, inputs, outputs);
+    }
+    catch(const std::exception& e) {
+        throw std::runtime_error(e.what());
+    }
+    return;
 }
 
-py::bytes PyClient::get_model(const std::string& key) {
-  std::string model = std::string(this->_client->get_model(key));
-  model = py::bytes(model);
-  return model;
+py::bytes PyClient::get_model(const std::string& key)
+{
+    std::string model;
+    try {
+        model = std::string(this->_client->get_model(key));
+    }
+    catch(const std::exception& e) {
+        throw std::runtime_error(e.what());
+    }
+    model = py::bytes(model);
+    return model;
 }
 
 void PyClient::set_model(const std::string& key,
@@ -156,10 +212,15 @@ void PyClient::set_model(const std::string& key,
                  const std::vector<std::string>& inputs,
                  const std::vector<std::string>& outputs)
 {
-  this->_client->set_model(key, model, backend, device,
-                           batch_size, min_batch_size, tag,
-                           inputs, outputs);
-  return;
+    try {
+        this->_client->set_model(key, model, backend, device,
+                                batch_size, min_batch_size, tag,
+                                inputs, outputs);
+    }
+    catch(const std::exception& e) {
+        throw std::runtime_error(e.what());
+    }
+    return;
 }
 
 void PyClient::set_model_from_file(const std::string& key,
@@ -172,18 +233,28 @@ void PyClient::set_model_from_file(const std::string& key,
                                    const std::vector<std::string>& inputs,
                                    const std::vector<std::string>& outputs)
 {
-  this->_client->set_model_from_file(key, model_file, backend, device,
-                                     batch_size, min_batch_size, tag,
-                                     inputs, outputs);
-  return;
+    try {
+        this->_client->set_model_from_file(key, model_file, backend, device,
+                                           batch_size, min_batch_size, tag,
+                                           inputs, outputs);
+    }
+    catch(const std::exception& e) {
+        throw std::runtime_error(e.what());
+    }
+    return;
 }
 
 void PyClient::run_model(const std::string& key,
                          std::vector<std::string> inputs,
                          std::vector<std::string> outputs)
 {
-  this->_client->run_model(key, inputs, outputs);
-  return;
+    try {
+        this->_client->run_model(key, inputs, outputs);
+    }
+    catch(const std::exception& e) {
+        throw std::runtime_error(e.what());
+    }
+    return;
 }
 
 void PyClient::set_data_source(const std::string& source_id)
