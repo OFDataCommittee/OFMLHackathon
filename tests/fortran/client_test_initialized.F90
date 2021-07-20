8e13934b53b039c110fd51a5387c1e2f43afd69c
Jim Edwards <jedwards@ucar.edu>
2021-07-20 13:25:55 -0600

    add a test for isinitialized

---
+++ tests/fortran/client_test_initialized.F90
@@ -0,0 +1,13 @@
+program main
+  use smartredis_client, only : client_type
+  use test_utils, only : use_cluster
+
+  type(client_type) :: client
+
+  if(client%isinitialized()) stop 'client not initialized'
+
+  call client%initialize(use_cluster())
+
+  if(.not. client%isinitialized()) stop 'client is initialized'
+
+end program main
