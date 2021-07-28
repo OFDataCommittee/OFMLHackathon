69a8d431f5a3854727d201ec547298f5c71502be
Matthew Ellis <matthew.ellis@hpe.com>
2021-07-28 09:48:52 -0700

    Add performance improvements to the RedisAI build.

--- build-scripts/build-redisai-cpu.sh
+++ build-scripts/build-redisai-cpu.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# get the number of processors
+NPROC=$(python -c "import multiprocessing as mp; print(mp.cpu_count())")
+
 #Install RedisAI
 if [[ -f ./RedisAI/install-cpu/redisai.so ]]; then
     echo "RedisAI CPU has already been downloaded and installed"
 else
     if [[ ! -d "./RedisAI" ]]; then
-        git clone --recursive https://github.com/RedisAI/RedisAI.git --branch v1.2.3 --depth=1 RedisAI
+        GIT_LFS_SKIP_SMUDGE=1 git clone --recursive https://github.com/RedisAI/RedisAI.git --branch v1.2.3 --depth=1 RedisAI
         cd RedisAI
         cd ..
     else
@@ -15,7 +18,7 @@ else
     echo "Downloading RedisAI CPU dependencies"
     CC=gcc CXX=g++ WITH_PT=1 WITH_TF=1 WITH_TFLITE=0 WITH_ORT=0 bash get_deps.sh cpu
     echo "Building RedisAI"
-    CC=gcc CXX=g++ GPU=0 WITH_PT=1 WITH_TF=1 WITH_TFLITE=0 WITH_ORT=0 WITH_UNIT_TESTS=0 make -C opt clean build
+    CC=gcc CXX=g++ GPU=0 WITH_PT=1 WITH_TF=1 WITH_TFLITE=0 WITH_ORT=0 WITH_UNIT_TESTS=0 make -j $NPROC -C opt clean build
 
     if [ -f "./install-cpu/redisai.so" ]; then
         echo "Finished installing RedisAI"
