cf894561d57aa913c94704def5446f9eed934c67
Julian Bissantz <bissantz@stfs.tu-darmstadt.de>
2022-07-25 17:35:42 +0200

    Initial implementation of fill_buffer to get minimal working example running

--- 2022-07/ofml-drl/src/python/buffer/local_buffer.py
+++ 2022-07/ofml-drl/src/python/buffer/local_buffer.py
@@ -1,9 +1,14 @@
 
 from os.path import join
+import os
 from shutil import copytree
 from copy import deepcopy
+import subprocess
+import _thread
+import queue
 import torch as pt
 from .buffer import Buffer
+import numpy as np
 
 
 class LocalBuffer(Buffer):
@@ -26,10 +31,37 @@ class LocalBuffer(Buffer):
         return envs
 
     def fill(self):
-        # set seed
+        # TODO: If fewer runners than buffer size, this needs reimplementation to be different for each buffer entry
+        for i, env in enumerate(self._envs):
+            env.seed = i
+        
         # run case
+        # get status of trajectory
+        results = queue.Queue()
+        process_count = 0
+        proc = []
+
+        # execute the n = n_workers trajectory simultaneously
+        # set the counter to count the number of trajectory
+        buffer_counter = 0
+        for n in np.arange(self._n_runners):
+            #self.run_trajectory(buffer_counter, proc, results, sample, action_bounds, env)
+            self.run_trajectory2(buffer_counter, proc, results, self._envs[buffer_counter])
+            process_count += 1
+            # increase the counter of trajectory number
+            buffer_counter += 1
+
         # fetch observations
-        pass
+        # check for any worker is done. if so give next trajectory to that worker
+        while process_count > 0:
+            job_name, rc = results.get()
+            print("job : ", job_name, "finished with rc =", rc)
+            if self.buffer_size > buffer_counter:
+                #self.run_trajectory(buffer_counter, proc, results, sample, action_bounds, env)
+                self.run_trajectory2(buffer_counter, proc, results, self._envs[buffer_counter])
+                process_count += 1
+                buffer_counter += 1
+            process_count -= 1
 
     def sample(self):
         return self._states, self._actions, self._rewards, self._log_p
@@ -43,3 +75,88 @@ class LocalBuffer(Buffer):
             env.reset()
         self._states, self._actions, self._rewards, self._log_p = [], [], [], []
 
+    # Added helper code
+    def process_waiter(self, proc, job_name, que):
+        """
+             This method is to wait for the executed process till it is completed
+         """
+        try:
+            proc.wait()
+        finally:
+            que.put((job_name, proc.returncode))
+
+    def run_trajectory(self, buffer_counter, proc, results, sample, action_bounds, env):
+        """
+        To run the trajectories
+        Args:
+            buffer_counter: which trajectory to run (n -> traj_0, traj_1, ... traj_n)
+            proc: array to hold process waiting flag
+            results: array to hold process finish flag
+            sample: number of iteration of main ppo
+            action_bounds: min and max omega value
+        Returns: execution of OpenFOAM Allrun file in machine
+        """
+        # # some hardcoded trajectory settings
+        # core_count = 2
+
+        # # make dir for new trajectory
+        # traj_path = f"./env/sample_{sample}/trajectory_{buffer_counter}"
+
+        # print(f"\n starting trajectory : {buffer_counter} \n")
+        # os.makedirs(traj_path, exist_ok=True)
+        # # copy files form base_case
+        # # change of ending time -> system/controlDict
+        # os.popen(
+        #     f'cp -r ./env/base_case/agentRotatingWallVelocity/* {traj_path}/ &&'
+        #     f'sed -i "s/timeStart.*/timeStart       4.01;/g" {traj_path}/system/controlDict &&'
+        #     f'sed -i "/^endTime/ s/endTime.*/endTime         5.0;/g" {traj_path}/system/controlDict'
+        # )
+        
+        # for i in range(core_count):
+        #     os.popen(
+        #         f'sed -i "s/startTime.*/startTime       4.009999;/g" {traj_path}/processor{i}/4/U &&'
+        #         f'sed -i "s/absOmegaMax.*/absOmegaMax       {action_bounds[1]};/g" {traj_path}/processor{i}/4/U'
+        #     )
+        # executing Allrun to start trajectory
+        proc[buffer_counter] = subprocess.Popen([f'{env.run_script}'], cwd=f'{env.path}/')
+        _thread.start_new_thread(self.process_waiter,
+                                 (proc[buffer_counter], f"trajectory_{buffer_counter}", results))
+
+    def run_trajectory2(self, buffer_counter, proc, results, env):
+        """
+        To run the trajectories
+        Args:
+            buffer_counter: which trajectory to run (n -> traj_0, traj_1, ... traj_n)
+            proc: array to hold process waiting flag
+            results: array to hold process finish flag
+            sample: number of iteration of main ppo
+            action_bounds: min and max omega value
+        Returns: execution of OpenFOAM Allrun file in machine
+        """
+        # # some hardcoded trajectory settings
+        # core_count = 2
+
+        # # make dir for new trajectory
+        # traj_path = f"./env/sample_{sample}/trajectory_{buffer_counter}"
+
+        # print(f"\n starting trajectory : {buffer_counter} \n")
+        # os.makedirs(traj_path, exist_ok=True)
+        # # copy files form base_case
+        # # change of ending time -> system/controlDict
+        # os.popen(
+        #     f'cp -r ./env/base_case/agentRotatingWallVelocity/* {traj_path}/ &&'
+        #     f'sed -i "s/timeStart.*/timeStart       4.01;/g" {traj_path}/system/controlDict &&'
+        #     f'sed -i "/^endTime/ s/endTime.*/endTime         5.0;/g" {traj_path}/system/controlDict'
+        # )
+        
+        # for i in range(core_count):
+        #     os.popen(
+        #         f'sed -i "s/startTime.*/startTime       4.009999;/g" {traj_path}/processor{i}/4/U &&'
+        #         f'sed -i "s/absOmegaMax.*/absOmegaMax       {action_bounds[1]};/g" {traj_path}/processor{i}/4/U'
+        #     )
+        print(f"CWD: {env.path}")
+        print(f"CWD absolute:{join(os.getcwd(), env.path)}")
+        # executing Allrun to start trajectory
+        proc[buffer_counter] = subprocess.Popen([f'wait.sh'], cwd=f'{join(os.getcwd(), env.path)}')
+        _thread.start_new_thread(self.process_waiter,
+                                 (proc[buffer_counter], f"trajectory_{buffer_counter}", results))
