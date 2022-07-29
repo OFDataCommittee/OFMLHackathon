c9574ed3b4267f3373c8d7538cc889a23caac878
Bissantz, Julian <bissantz@stfs.tu-darmstadt.de>
2022-07-29 11:43:48 +0200

    Updated run-script with argparse to make it more versitile running from the command-line

--- 2022-07/ofml-drl/run_training.py
+++ 2022-07/ofml-drl/run_training.py
@@ -1,6 +1,7 @@
 
-from src.python.buffer import LocalBuffer
+from src.python.buffer import LocalBuffer, SlurmBuffer
 from src.python.agent import PPOAgent
+from src.python.agentBayes import PPOAgentBayesian
 from src.python.environment import RotatingCylinder2D
 import pickle
 from shutil import copytree
@@ -9,6 +10,27 @@ from os import makedirs
 from os.path import join
 from typing import List
 from torch import Tensor
+import argparse
+
+def parseArguments():
+
+    ag = argparse.ArgumentParser()
+    ag.add_argument('-d', '--directory',
+                    help='Specify case directory, i.e. training path.')
+    ag.add_argument('-a','--agent', choices=['PPOAgent', 'PPOAgentBayesian'], required=False, default='PPOAgent',
+                    help='Select agent type, default PPOAgent')
+    ag.add_argument('-n', '--nrunner', type=int, required=False, default=10,
+                    help='Specify number of runners for training, default=10')
+    ag.add_argument('-s', '--buffer-size', type=int, required=False, default=10,
+                    help='Specify buffer-size of training, default=10')
+    ag.add_argument('-e', '--episodes', type=int, required=False, default=10,
+                    help='Specify number of episodes for training, default=10')
+    ag.add_argument('-t', '--end-time', type=float, required=False, default=6.,
+                    help='Specify end time of simulation, i.e. trajectory length, default=6.0')
+    ag.add_argument('-b', '--buffer-type', type=str, choices=['local', 'slurm'], required=False, default='local',
+            help='Specify buffer type to use for training: Local = Local on your machine, slurm = submit job to slurm; default=local')
+    args = ag.parse_args()
+    return args
 
 
 def print_statistics(actions: List[Tensor], rewards: List[Tensor]):
@@ -22,14 +44,14 @@ def print_statistics(actions: List[Tensor], rewards: List[Tensor]):
           len(at_std), min(at_std), max(at_std))
 
 
-def main():
+def main(args):
 
     # setting
-    training_path = "test_training"
-    episodes = 10
-    buffer_size = 10
-    n_runners = 10
-    end_time = 10.0
+    training_path = args.directory
+    episodes = args.episodes
+    buffer_size = args.buffer_size
+    n_runners = args.n_runners
+    end_time = args.end_time
 
     # create a directory for training
     makedirs(training_path, exist_ok=True)
@@ -44,10 +66,13 @@ def main():
     env.reset()
 
     # create a trajectory buffer
-    buffer = LocalBuffer(training_path, env, buffer_size, n_runners)
+    buffer = LocalBuffer(training_path, env, buffer_size, n_runners) if args.buffer_type == "local" \
+                else SlurmBuffer(training_path, env, buffer_size, n_runners)
 
     # create PPO agent
     agent = PPOAgent(env.n_states, env.n_actions, -
+                     env.action_bounds, env.action_bounds) if args.agent == "PPOAgent" else \
+            PPOAgentBayesian(env.n_states, env.n_actions, -
                      env.action_bounds, env.action_bounds)
 
     # begin training
@@ -73,4 +98,5 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    args = parseArguments()
+    main(args)
